bc1c1ca371a4dea4c562851dd6ae004de19d598e
叶桂城 <1483249153@qq.com>
2020-04-29 17:29:18 +0800

    完成页面的基本配置

---
+++ vue-admin/src/views/login/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="login">
+    <h1>这是登录页面</h1>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style lang='scss' scoped>
+#login {
+  height: 100vh;
+  background: #344a5f;
+}
+</style>
