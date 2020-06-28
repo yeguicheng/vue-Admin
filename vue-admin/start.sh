4ed187467934c77dedbf882779272dc0d0951ca0
叶桂城 <1483249153@qq.com>
2020-06-28 17:25:41 +0800

    设置最高权限执行

--- vue-admin/start.sh
+++ vue-admin/start.sh
@@ -1,11 +1,16 @@
 #拉取代码
 git pull
 #安装依赖
-npm install --registry=https://registry.npm.taobao.org/ && npm run build
+sudo yarn --registry=https://registry.npm.taobao.org/ && yarn build
+# sudo npm install --registry=https://registry.npm.taobao.org/ && npm run build
 #删除容器
 docker rm -f Vue-Admin &> /dev/null
 #启动容器
-docker run -d --restart=no-failure:5 \
--p 8081:80 \
--v $PWD\dist:/usr/share/nginx/html \
---name Vue-Admin nginx
+docker run -d --restart=on-failure:5\
+    -p 8081:80 \
+    -v $PWD/dist:/usr/share/nginx/html \
+    --name Vue-Admin nginx
+// docker run -d --restart=no-failure:5 \
+// -p 8081:80 \
+// -v $PWD/dist:/usr/share/nginx/html \
+// --name Vue-Admin nginx
